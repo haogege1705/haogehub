d43e3f1f2411b2cdfb3f0b05e4281bc9a76dc51b
haogege1705 <534751537@qq.com>
2022-03-27 16:15:35 +0800

    修改图片地址

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -19,7 +19,7 @@ class MomentService {
          JSON_OBJECT('id', c.id, 'content', c.content, 'commentId', c.comment_id, 'createTime', c.createAt,
                      'user', JSON_OBJECT('id', cu.id, 'name', cu.name,'avatar', cu.avatar_url))
                     ),NULL) FROM comment c LEFT JOIN users cu ON c.user_id = cu.id WHERE m.id = c.moment_id) comments,
-       (SELECT JSON_ARRAYAGG(CONCAT('http://localhost:8000/moment/images/', file.filename)) FROM file WHERE m.id = file.moment_id) image
+       (SELECT JSON_ARRAYAGG(CONCAT('http://121.41.115.226:8000/moment/images/', file.filename)) FROM file WHERE m.id = file.moment_id) image
        FROM moment m
        LEFT JOIN users u ON m.user_id = u.id
        LEFT JOIN moment_label ml ON m.id = ml.moment_id
@@ -39,7 +39,7 @@ class MomentService {
       JSON_OBJECT('user_id', users.id, 'name', users.name,'avatar', users.avatar_url) userVO,
       (SELECT COUNT(*) FROM comment WHERE comment.moment_id  =  moment.id) commentCount,
       (SELECT COUNT(*) FROM moment_label WHERE moment_label.moment_id  =  moment.id) labelCount,
-      (SELECT JSON_ARRAYAGG(CONCAT('http://localhost:8000/moment/images/', file.filename)) FROM file WHERE moment.id = file.moment_id) image
+      (SELECT JSON_ARRAYAGG(CONCAT('http://121.41.115.226:8000/moment/images/', file.filename)) FROM file WHERE moment.id = file.moment_id) image
       FROM moment LEFT JOIN users ON moment.user_id = users.id LIMIT ?, ?;`
     const [result] = await connection.execute(statement, [offset, size]);
     return result;
